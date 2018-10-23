adb30d50c0fd86d64837aec7193584ba4bdd299c
David Barnes <dbarnes@kvcc.edu>
2018-10-23 17:47:01 -0400

    Add IntegerLinkedList interface. Add IntegerLinkedList class. Finish 4 main methods for the linked list. Add some test lines to Program.cs.

---
+++ cis237-inclass4/IIntegerLinkedList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237_inclass4
+{
+    interface IIntegerLinkedList
+    {
+        void AddToFront(int IntegerData);
+        void AddToBack(int IntegerData);
+        int RemoveFromFront();
+        int RemoveFromBack();
+
+        void Display();
+
+        bool IsEmpty { get; }
+        int Size { get; }
+    }
+}
--- cis237-inclass4/Program.cs
+++ cis237-inclass4/Program.cs
@@ -10,6 +10,25 @@ namespace cis237_inclass4
     {
         static void Main(string[] args)
         {
+            IIntegerLinkedList linkedList = new IntegerLinkedList();
+
+            linkedList.AddToFront(5);
+            linkedList.AddToFront(6);
+            linkedList.AddToFront(3);
+
+            linkedList.Display();
+
+            linkedList.AddToBack(2);
+
+            linkedList.Display();
+
+            linkedList.RemoveFromFront();
+
+            linkedList.Display();
+
+            linkedList.RemoveFromBack();
+
+            linkedList.Display();
         }
     }
 }
